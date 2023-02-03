5b6b88d3dc466e8e9cfe77082bccc6e7392ee370
rahular555 <rahular@leuwint.com>
2023-02-03 15:11:56 +0530

    Updated 03/02/2023

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+import { FormsModule } from '@angular/forms'
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
